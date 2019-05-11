81285d8786f895bc792a4d6e2d7bc5fdb0726d2d
Pavel Ramasheuski <pavel.ramasheuski@gmail.com>
2019-05-11 20:54:33 +0300

    Update

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist'));
-
+app.use(express.static(__dirname + './dist'));
+app.get(/.*/, function(req,res){
+  res.sendFile(__dirname+'/dist/index.html')
+})
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
